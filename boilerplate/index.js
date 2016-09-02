4a151ee5eb00e9e81362cc42f5e665ed70e7e24d
TZ | 天猪 <atian25@qq.com>
2016-09-03 00:10:03 +0800

    feat: add agent (#2)

--- boilerplate/index.js
+++ boilerplate/index.js
@@ -1,15 +1,3 @@
 'use strict';
 
-const path = require('path');
-const egg = require('egg');
-const EGG_PATH = Symbol.for('egg#eggPath');
-
-class Application extends egg.Application {
-  get [EGG_PATH]() {
-    return path.join(__dirname);
-  }
-}
-
-module.exports = Object.assign({}, egg, {
-  Application,
-});
+module.exports = require('./lib/framework.js');
